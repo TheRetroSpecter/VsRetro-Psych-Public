daaeea9b196de6742f9f7e9440535c9949e99b5a
MaysLastPlay <107422651+MaysLastPlayGithub@users.noreply.github.com>
2022-10-14 21:30:57 +0300

    Stuff

--- source/options/OptionsState.hx
+++ source/options/OptionsState.hx
@@ -34,14 +34,29 @@ class OptionsState extends MusicBeatState
 	function openSelectedSubstate(label:String) {
 		switch(label) {
 			case 'Note Colors':
+				#if android
+			        removeVirtualPad();
+			        #end
 				openSubState(new options.NotesSubState());
 			case 'Controls':
+				#if android
+			        removeVirtualPad();
+			        #end
 				openSubState(new options.ControlsSubState());
 			case 'Graphics':
+				#if android
+			        removeVirtualPad();
+			        #end
 				openSubState(new options.GraphicsSettingsSubState());
 			case 'Visuals and UI':
+				#if android
+			        removeVirtualPad();
+			        #end
 				openSubState(new options.VisualsUISubState());
 			case 'Gameplay':
+				#if android
+			        removeVirtualPad();
+			        #end
 				openSubState(new options.GameplaySettingsSubState());
 			case 'Adjust Delay and Combo':
 				if (TitleState.introMusic != null && TitleState.introMusic.playing)
@@ -91,6 +106,11 @@ class OptionsState extends MusicBeatState
 		changeSelection();
 		ClientPrefs.saveSettings();
 
+		#if android
+		addVirtualPad(UP_DOWN, A_B_C);
+		virtualPad.y = -24;
+		#end
+			
 		super.create();
 	}
 
@@ -117,6 +137,15 @@ class OptionsState extends MusicBeatState
 		if (controls.ACCEPT) {
 			openSelectedSubstate(options[curSelected]);
 		}
+		
+		#if android
+		if (virtualPad.buttonC.justPressed) {
+			#if android
+			removeVirtualPad();
+			#end
+			openSubState(new android.AndroidControlsSubState());
+		}
+		#end
 	}
 	
 	function changeSelection(change:Int = 0) {
