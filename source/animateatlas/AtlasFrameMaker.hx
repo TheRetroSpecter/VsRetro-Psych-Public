4099878b978326b61b81a0e26c13b3554024fa49
MaysLastPlay <107422651+MaysLastPlayGithub@users.noreply.github.com>
2022-10-14 21:49:29 +0300

    Add files via upload

--- source/animateatlas/AtlasFrameMaker.hx
+++ source/animateatlas/AtlasFrameMaker.hx
@@ -17,9 +17,6 @@ import flixel.graphics.frames.FlxFrame;
 #if desktop
 import sys.FileSystem;
 import sys.io.File;
-#else
-import js.html.FileSystem;
-import js.html.File;
 #end
 
 using StringTools;
